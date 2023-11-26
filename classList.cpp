b0591e999aa4049931b0041cba026f9a9aef4387
AndreyMustafin2001 <andrey.must2001@gmail.com>
2023-11-26 17:19:13 +0400

    Added PopFront , PopBack
     Added elementscount ,GetSize,
     PopFront , PopBack
     and tested for input and output
     elements

--- classList.cpp
+++ classList.cpp
@@ -16,6 +16,8 @@ private:
     Node* first = nullptr;
     Node* last = nullptr;
 
+    int elementsCount = 0;
+
 public:
 
     List() = default;
@@ -34,6 +36,7 @@ public:
             last->next = node;
             last = node;
         }
+        ++elementsCount;
     }
 
     void PushFront(const T elem)
@@ -50,7 +53,50 @@ public:
             first->prev = node;
             first = node;
         }
+        ++elementsCount;
 
+    }
+
+    void PopBack()
+    {
+        if (last != nullptr)
+        {
+            Node* temp = last;
+            last = last->prev;
+
+            if (last != nullptr)
+            {
+                last->next = nullptr; 
+            }
+
+            else
+            {
+                first = nullptr;
+            }
+
+            delete temp;
+            elementsCount--;
+        }
+    }
+
+    void PopFront()
+    {
+        if (first != nullptr)
+        {
+            Node* temp = first;
+            first = first->next;
+
+            if (first != nullptr)
+            {
+                first->prev = nullptr;
+            }
+            else
+            {
+                last = nullptr;
+            }
+            delete temp;
+            elementsCount--;
+        }
     }
     //Move to first node
     const T& Front()const
@@ -62,6 +108,16 @@ public:
     {
         return last->elem;
     }
+    //Get size of a list
+    size_t GetSize()const
+    {
+        return elementsCount;
+    }
+    //Check if node is empty
+    bool Empty()const
+    {
+        return first == nullptr;
+    }
 };
 
 int main()
@@ -69,12 +125,18 @@ int main()
     List<int> lst;
 
     lst.PushFront(1);
+    lst.PushBack(7);
+    lst.PushFront(2);
+    lst.PushFront(3);
+    lst.PushFront(5);
 
-    std::cout << lst.Front() << "\n";
+    std::cout << "Count of elements: " << lst.GetSize() << std::endl;
 
-    lst.PushBack(2);
-
-    std::cout << lst.Back() << "\n";
+    while (!lst.Empty())
+    {
+        std::cout << lst.Front() << " ";
+        lst.PopFront();
+    }
 
     return 0;
 }
